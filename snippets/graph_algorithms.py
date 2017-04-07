2dbe2246676b28ce1a1eb38e52e932b5e304b9d0
letyrodri <lrodrig@dc.uba.ar>
2017-04-07 17:43:23 -0300

    adding gitignore

--- snippets/graph_algorithms.py
+++ snippets/graph_algorithms.py
@@ -7,44 +7,46 @@
 # Shortest path from one node to the rest
 # Positive Weighted Graph
 
+
 def dijkstra(G, w, s):
-	d = []  # distances array
-	p = []  # parents array
+    d = []  # distances array
+    p = []  # parents array
 
-	for v in range(0,len(G)):
-		d.append(float("Inf"))
-		p.append(None)
+    for v in range(0, len(G)):
+        d.append(float("Inf"))
+        p.append(None)
 
-	# the distance for starting node is the same
-	d[s] = 0
+    # the distance for starting node is the same
+    d[s] = 0
 
-	S = set()
-	Q = [( d[x], x ) for x in range(1,len(G)) ]
+    S = set()
+    Q = [(d[x], x) for x in range(1, len(G))]
 
-	while (Q):
-		e = min(Q)
-		u = e[1]
-		Q.remove(e)
+    while (Q):
+        e = min(Q)
+        u = e[1]
+        Q.remove(e)
 
-		# iterates u adyacents
-		for v in G[u]:
-			if d[v] > d[u]+w[u][v]:
-				Q.remove((d[v],v))
-				d[v] = d[u]+w[u][v]
-				p[v] = u
-				Q.append( (d[v],v))
+        # iterates u adyacents
+        for v in G[u]:
+            if d[v] > d[u] + w[u][v]:
+                Q.remove((d[v], v))
+                d[v] = d[u] + w[u][v]
+                p[v] = u
+                Q.append((d[v], v))
 
-	return d, p
+    return d, p
 
-def get_path(p,s,e):
-	node = e
-	res = []
 
-	while (not node == s):
-		res.append(node)
-		node = p[node]
+def get_path(p, s, e):
+    node = e
+    res = []
 
-	res.append(s)
-	res.reverse()
+    while not node == s:
+        res.append(node)
+        node = p[node]
 
-	return res
+    res.append(s)
+    res.reverse()
+
+    return res
